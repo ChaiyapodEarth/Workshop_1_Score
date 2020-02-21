64a1804fc3e63015ac4db5667dec9ea85d628a6a
kittinun chobtham <chobtham.kittinun@gmail.com>
2020-02-22 00:22:46 +0700

    Init project.

---
+++ Workshop_1_Score/Controllers/ScoreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScoreViewController.swift
+//  Workshop_1_Score
+//
+//  Created by Kittinun Chobtham on 22/2/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
