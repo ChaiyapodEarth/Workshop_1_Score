c5146ca9bdf3d2eadb86c5f8870cc5285c9c833a
kittinun chobtham <chobtham.kittinun@gmail.com>
2020-02-22 00:25:13 +0700

    Added outlets textfields to scoreviewcontroller file.

--- Workshop_1_Score/Controllers/ScoreViewController.swift
+++ Workshop_1_Score/Controllers/ScoreViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class ScoreViewController: UIViewController {
 
+    @IBOutlet weak var scoreTextField: UITextField!
+    @IBOutlet weak var gradeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func convertScoreToGradeTapped(_ sender: Any?) {
     }
-    */
-
+    
 }
